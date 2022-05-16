86aabc3b7715b6defb3b8a0d2dcde1851eb607d9
ShaneWilmes <shanewilmes@yahoo.com>
2022-05-16 16:51:13 -0600

    adding code to route files

---
+++ routes/apiRoutes.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const uuid = require('uuid');
+const router = require('express').Router()
+
+// Acquiring data for the note
+app.get('/notes', (req, res) => {
+    const data = fs.readFileSync('./db/db.json');
+    res.json(JSON.parse(data));
+})
+
+// Posts the data to the website
+app.post('/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+    const newNote = req.body;
+    newNote.id = uuid.v4();
+    notes.push(newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    res.json(notes);
+})
+
+// Bonus:  adding a delete notes option
+
+app.delete('/notes/:id', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'))
+    const deleteNote= notes.filter((eraseNote) => eraseNote.id !== req.params.id);
+    fs.writeFileSync('./db/db.json', JSON.stringify(deleteNote));
+    res.json(deleteNote);
+})
+
+
