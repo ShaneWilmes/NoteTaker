a01f65867b56cb825d72f7202e0a5ca1fb1f2d8c
ShaneWilmes <shanewilmes@yahoo.com>
2022-05-16 17:11:45 -0600

    installed uuid and added code for html routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,32 @@
 const fs = require('fs');
 const uuid = require('uuid');
-const router = require('express').Router()
+const router = require('express').Router();
 
 // Acquiring data for the note
-app.get('/notes', (req, res) => {
+router.get('/notes', (req, res) => {
     const data = fs.readFileSync('./db/db.json');
     res.json(JSON.parse(data));
-})
+});
 
 // Posts the data to the website
-app.post('/notes', (req, res) => {
+router.post('/notes', (req, res) => {
     const notes = JSON.parse(fs.readFileSync('./db/db.json'));
     const newNote = req.body;
     newNote.id = uuid.v4();
     notes.push(newNote);
     fs.writeFileSync('./db/db.json', JSON.stringify(notes));
     res.json(notes);
-})
+});
 
 // Bonus:  adding a delete notes option
 
-app.delete('/notes/:id', (req, res) => {
+router.delete('/notes/:id', (req, res) => {
     const notes = JSON.parse(fs.readFileSync('./db/db.json'))
     const deleteNote= notes.filter((eraseNote) => eraseNote.id !== req.params.id);
     fs.writeFileSync('./db/db.json', JSON.stringify(deleteNote));
     res.json(deleteNote);
-})
+});
+
+module.exports = router;
 
 
