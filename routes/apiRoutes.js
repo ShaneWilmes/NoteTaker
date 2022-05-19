6c8d4d97a1d7edd2c56bc424cef2627bc44f683c
ShaneWilmes <shanewilmes@yahoo.com>
2022-05-18 19:16:41 -0600

    fixed a few bugs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,26 +4,26 @@ const router = require('express').Router();
 
 // Acquiring data for the note
 router.get('/notes', (req, res) => {
-    const data = fs.readFileSync('./db/db.json');
+    const data = fs.readFileSync('db/db.json');
     res.json(JSON.parse(data));
 });
 
 // Posts the data to the website
 router.post('/notes', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+    const notes = JSON.parse(fs.readFileSync('db/db.json'));
     const newNote = req.body;
     newNote.id = uuid.v4();
     notes.push(newNote);
-    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    fs.writeFileSync('db/db.json', JSON.stringify(notes));
     res.json(notes);
 });
 
 // Bonus:  adding a delete notes option
 
 router.delete('/notes/:id', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync('./db/db.json'))
+    const notes = JSON.parse(fs.readFileSync('db/db.json'))
     const deleteNote= notes.filter((eraseNote) => eraseNote.id !== req.params.id);
-    fs.writeFileSync('./db/db.json', JSON.stringify(deleteNote));
+    fs.writeFileSync('db/db.json', JSON.stringify(deleteNote));
     res.json(deleteNote);
 });
 
